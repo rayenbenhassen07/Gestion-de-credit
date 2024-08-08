1dfa286126e590adaa919ae9c9323b9cc84118f7
rayen benhassen <rayenbenhassen07@gmail.com>
2024-08-08 15:00:17 +0100

    testetst

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -15,7 +15,7 @@ export default function Test({ currentUser }) {
   }, [currentUser, isLoggedIn, router]);
 
   return (
-    <div className="p-4">
+    <div className="p-4 flex justify-start items-center gap-2">
       <div
         onClick={() => signOut()}
         className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700 cursor-pointer w-32 "
