81c46644f0e5d70317c60c1fb22b0ae11509998b
rayen benhassen <rayenbenhassen07@gmail.com>
2024-08-08 12:03:20 +0100

    qwwwwwww

--- src/app/transactions/[clientId]/page.jsx
+++ src/app/transactions/[clientId]/page.jsx
@@ -187,10 +187,7 @@ export default function ClientPage({ params }) {
                   <TableBody>
                     {filteredTransactions.length > 0 ? (
                       filteredTransactions
-                        .sort(
-                          (a, b) =>
-                            new Date(b.created_at) - new Date(a.created_at)
-                        )
+                        .sort((a, b) => new Date(a.date) - new Date(b.date))
                         .map((transaction) => (
                           <TableRow key={transaction.id}>
                             <TableCell className="p-3 lg:p-2 text-xs lg:text-base w-1/6">
